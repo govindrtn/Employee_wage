6ee223f21ce70bbfb3d03c3e9d0090a86b5c8560
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-27 18:08:03 +0530

    Added: employee wages for a month

--- employeewage.java
+++ employeewage.java
@@ -4,12 +4,15 @@ public class employeewage {
 public static void main(String[] args) {
 	
 	final int PartTime=1;
-	final int FullTime=2;
-		int employeeCheck=(int) (Math.random() * 10)%3;
+    final int FullTime=2;
 	final int wagePerHrs=20;
-		int workingHrs=0;
-		
-			
+	final int workingDays=20;		
+		int totalwage =0;
+		for(int day=1; day<=workingDays;day++)
+		{
+			int employeeCheck=(int) (Math.random() * 10)%3;
+			int workingHrs=0;
+				
 		switch(employeeCheck) 
 		{
 		case FullTime:
@@ -25,5 +28,9 @@ public static void main(String[] args) {
 		}
 		int employeeWage = workingHrs*wagePerHrs;
 		System.out.println("Employee Daily wages is as per working hour " + employeeWage);
-	}
-	}
+		totalwage = totalwage+employeeWage;
+		}
+		System.out.println("Wage for a month "+ totalwage);
+}
+
+}

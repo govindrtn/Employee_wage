4c314b938377640c924c3df788f4fa670be4ff8b
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-27 18:04:35 +0530

    Added: employee using switch case

--- employeewage.java
+++ employeewage.java
@@ -3,25 +3,27 @@ package employeecomoutation;
 public class employeewage {
 public static void main(String[] args) {
 	
-	int PartTime=1;
-	int FullTime=2;
-	double employeeCheck=(int) (Math.random() * 10)%3;
-	int wagePerHrs=20;
-	int workingHrs=0;
-	
+	final int PartTime=1;
+	final int FullTime=2;
+		int employeeCheck=(int) (Math.random() * 10)%3;
+	final int wagePerHrs=20;
+		int workingHrs=0;
 		
-	if(employeeCheck==FullTime) {
-		System.out.println("Employee is present as 'FULLTIME' ");
-		workingHrs=8;
+			
+		switch(employeeCheck) 
+		{
+		case FullTime:
+			System.out.println("Employee is present as 'FULLTIME' ");
+			workingHrs=8;
+			break;
+		case PartTime:
+			System.out.println("Employee is present as 'PARTTIME' ");
+			workingHrs=4;
+			break;
+		default:
+			System.out.println("Employee is absent");
+		}
+		int employeeWage = workingHrs*wagePerHrs;
+		System.out.println("Employee Daily wages is as per working hour " + employeeWage);
 	}
-	else if(employeeCheck==PartTime){
-		System.out.println("Employee is present as 'PARTTIME' ");
-		workingHrs=4;
 	}
-	else {
-		System.out.println("Employee is absent");
-	}
-	int employeeWage = workingHrs*wagePerHrs;
-	System.out.println("Employee Daily wages is as per working hour " + employeeWage);
-}
-}

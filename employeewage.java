72999b69e7375ba7141075d1f57441dc7b86e3b3
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-27 17:54:37 +0530

    Added: Employee daily wages

--- employeewage.java
+++ employeewage.java
@@ -4,17 +4,21 @@ public class employeewage {
 public static void main(String[] args) {
 	
 	int Employee_present=1;
+	int wagePerHrs=20;
+	int workingHrs=8;
 	double employeeCheck=(int) (Math.random() * 10)%2;
 	
-	if (employeeCheck==Employee_present){
-		System.out.println("Employee is 'PRESENT'");
+	if(employeeCheck==Employee_present) {
+		System.out.println("Employee is 'PRESENT' ");
+		int employeeWage=wagePerHrs*workingHrs;
+		System.out.println("employee wage per working hours " + employeeWage);
 	}
 	else {
-		System.out.println("Employee is 'ABSENT'");
+		System.out.println("Employee is absent");
+		System.out.println("employee wage per working hours " + 0 + " beacuse employee is absent");
+
 	}
 
 }
 
 }
-	
-	

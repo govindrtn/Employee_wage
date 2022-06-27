32fec8a4335a708db337f94c4b7d37fd8072e415
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-27 18:32:33 +0530

    Added: employee total working hours

--- employeewage.java
+++ employeewage.java
@@ -6,12 +6,14 @@ public static void main(String[] args) {
 	final int PartTime=1;
     final int FullTime=2;
 	final int wagePerHrs=20;
-	final int workingDays=20;		
+	final int maxWorkingDays=20;
+	final int maxWorkingHrs=100;
 		int totalwage =0;
-		for(int day=1; day<=workingDays;day++)
+		int workingHrs=0;
+		
+		for(int day=1, totalWorkingHours =0; day <= maxWorkingDays && totalWorkingHours < maxWorkingHrs; day++ )
 		{
 			int employeeCheck=(int) (Math.random() * 10)%3;
-			int workingHrs=0;
 				
 		switch(employeeCheck) 
 		{
@@ -27,8 +29,9 @@ public static void main(String[] args) {
 			System.out.println("Employee is absent");
 		}
 		int employeeWage = workingHrs*wagePerHrs;
-		System.out.println("Employee Daily wages is as per working hour " + employeeWage);
 		totalwage = totalwage+employeeWage;
+		totalWorkingHours = totalWorkingHours + workingHrs;
+		System.out.println("Employee Daily wages is as per working hour " + employeeWage + " total working hours "+ totalWorkingHours);
 		}
 		System.out.println("Wage for a month "+ totalwage);
 }

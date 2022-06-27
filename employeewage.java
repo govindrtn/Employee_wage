a792b0c7407d3fa7b5b1e5d03f8b87500fd2f4b1
Govind maithil <govindmaithilrtn@gmail.com>
2022-06-27 17:49:17 +0530

    Added: Employee present or absent

---
+++ employeewage.java
@@ -0,0 +1,20 @@
+package employeecomoutation;
+
+public class employeewage {
+public static void main(String[] args) {
+	
+	int Employee_present=1;
+	double employeeCheck=(int) (Math.random() * 10)%2;
+	
+	if (employeeCheck==Employee_present){
+		System.out.println("Employee is 'PRESENT'");
+	}
+	else {
+		System.out.println("Employee is 'ABSENT'");
+	}
+
+}
+
+}
+	
+	

8281669f469b1b86c69703c9aed0dd322c7d62d1
Govind maithil <govindmaithilrtn@gmail.com>
2022-07-06 10:42:10 +0530

    Added: Calculating Employee wage using OPPs

--- employeewage.java
+++ employeewage.java
@@ -1,55 +1,45 @@
 package employeecomoutation;
 
-public class employeewage {
+public class employeewage 
+{
+public static void CalculateWage()
+{
 	
-	public static final int PartTime = 1;
-	public static final int FullTime = 2;
-    public int wagePerHrs;
-	public int maxWorkingDays; 
-	public int maxWorkingHrs;
-	
-	public employeewage(int wagePerHours , int maxWorkingDays , int maxWorkingHrs) {
+	final int PartTime=1;
+    final int FullTime=2;
+	final int wagePerHrs=20;
+	final int maxWorkingDays=20;
+	final int maxWorkingHrs=100;
+		int totalwage =0;
+		int workingHrs=0;
 		
-		this.wagePerHrs = wagePerHours;
-		this.maxWorkingDays = maxWorkingDays;
-		this.maxWorkingHrs = maxWorkingHrs;
-		
-	}
-	public static void getEmployeeWageFunction() 
-	{
-		  int totalwage = 0;
-	      int workingHrs = 0;
-
-	      employeewage myObj = new employeewage(20,20,100); 
-	      
-	      for(int day = 1, totalWorkingHours = 0; day <= myObj.maxWorkingDays && totalWorkingHours < myObj.maxWorkingHrs; day++ )
-			{
-				int employeeCheck=(int) (Math.random() * 10)%3;
-					
-			switch(employeeCheck) 
-			{
-			case FullTime:
-				System.out.println("Employee is present as 'FULLTIME' ");
-				workingHrs=8;
-				break;
-			case PartTime:
-				System.out.println("Employee is present as 'PARTTIME' ");
-				workingHrs=4;
-				break;
-			default:
-				System.out.println("Employee is absent");
-			}
-			int employeeWage = workingHrs*myObj.wagePerHrs;
-			totalwage = totalwage+employeeWage;
-			totalWorkingHours = totalWorkingHours + workingHrs;
-			System.out.println("Employee Daily wages is as per working hour " + employeeWage + " total working hours "+ totalWorkingHours);
-			}
-			System.out.println("Wage for a month "+ totalwage);
+		for(int day=1, totalWorkingHours =0; day <= maxWorkingDays && totalWorkingHours < maxWorkingHrs; day++ )
+		{
+			int employeeCheck=(int) (Math.random() * 10)%3;
 				
+		switch(employeeCheck) 
+		{
+		case FullTime:
+			System.out.println("Employee is present as 'FULLTIME' ");
+			workingHrs=8;
+			break;
+		case PartTime:
+			System.out.println("Employee is present as 'PARTTIME' ");
+			workingHrs=4;
+			break;
+		default:
+			System.out.println("Employee is absent");
 		}
+		int employeeWage = workingHrs*wagePerHrs;
+		totalwage = totalwage+employeeWage;
+		totalWorkingHours = totalWorkingHours + workingHrs;
+		System.out.println("Employee Daily wages is as per working hour " + employeeWage + " total working hours "+ totalWorkingHours);
+		}
+		System.out.println("Wage for a month "+ totalwage);
+}
+public static void main(String[] args) {
+	CalculateWage();
 	
-	public static void main(String[] args) {
-		employeewage.getEmployeeWageFunction();
-		
-	}
+}
+
 }
